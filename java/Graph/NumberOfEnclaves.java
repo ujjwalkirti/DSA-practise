4596ac61ff486b21760b803251419ec165c8456f
ujjwalkirti <kirti.chargers@gmail.com>
2025-10-26 21:02:27 +0530

    improve: folder structure

---
+++ java/Graph/NumberOfEnclaves.java
@@ -0,0 +1,13 @@
+class NumberOfEnclaves{
+    public static void main(String[] args) {
+        int[][] grid = { { 0, 0, 0, 0 }, { 1, 0, 1, 0 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 } };
+        NumberOfEnclaves obj = new NumberOfEnclaves();
+        System.out.println(obj.numEnclaves(grid));
+    }
+
+    public int numEnclaves(int[][] grid) {
+        int ans = 0;
+        
+        return ans;
+    }
+}
